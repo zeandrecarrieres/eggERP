c9811ce8ec1d0ec453621859ab4f1e37e5cb16de
Zé Carrières <zeandredev@gmail.com>
2021-05-02 16:47:01 +0200

    cors http

--- server.js
+++ server.js
@@ -2,45 +2,30 @@
 // Express (Server) / Mongoose (MongoDB Connection) / Cors (Server permissions access)
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-//Server, enviroment variables and middlewares initialization 
-require('dotenv').config()
+//Server, enviroment variables and middlewares initialization
+require("dotenv").config();
 
 //Routes files links
-const clientRoutes =require('./routes/client.routes')
-const productRoutes =require('./routes/product.routes')
-const transactionRoutes =require('./routes/transaction.routes')
-const userRoutes =require('./routes/user.routes')
-
-//teste
-
+const clientRoutes = require("./routes/client.routes");
+const productRoutes = require("./routes/product.routes");
+const transactionRoutes = require("./routes/transaction.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
-app.use(cookieParser())
-// app.use((req, res, next)=>{
-//   res.header("Access-Control-Allow-Origin", "https://eggerp-frontend.herokuapp.com")
-//   app.use(cors())
-//   next()
-// })
-
+app.use(cookieParser());
 
 const corsOptions = {
   // origin:"http://localhost:3000",
-  origin:"https://nutriovosapp-frontend.herokuapp.com/",
+  origin: ["https://nutriovosapp-frontend.herokuapp.com","https://nutriovosapp-frontend.herokuapp.com"],
   credentials: true,
-}
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-// app.use(cors({
-//   credentials:true,
-//   origin: ['*']
-//   // origin: ['http://localhost:3001', 'http://localhost:3000', 'https://eggerp-frontend.herokuapp.com/']
-// }));
 app.use(express.json());
-
 
 //DB Connection (MongoDB)
 mongoose.connect(process.env.MONGO_URL, {
@@ -49,13 +34,13 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 });
 
-
 //Routes
-app.use('/client', clientRoutes)
-app.use('/product', productRoutes)
-app.use('/transaction', transactionRoutes)
-app.use('/user', userRoutes)
-
+app.use("/client", clientRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
+app.use("/user", userRoutes);
 
 //Enable PORT selected
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
